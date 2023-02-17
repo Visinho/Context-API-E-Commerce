80d186eb6503f128eb5b15a4d82f753fd2d7047f
Visinho <Elvisvisinho@rocketmail.com>
2023-02-17 10:24:20 +0100

    Added the Faker API and collected data in the Context

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { CartState } from '../context/Context'
+
 
 const Home = () => {
+
+    const { state: {products} } = CartState();
+
+    console.log(products);
+
   return (
-    <div>
-      Home
+    <div className='home'>
+      {/* <Filter /> */}
+        <div>
+            {
+                products.map((prod) => {
+                   return <span>{prod.name}</span>
+                })
+            }
+        </div>
     </div>
   )
 }
--- src/context/Context.js
+++ src/context/Context.js
@@ -1,5 +1,6 @@
-import { createContext } from 'react';
+import { createContext, useContext, useReducer } from 'react';
 import { faker } from '@faker-js/faker';
+import { cartReducer } from './Reducers';
 
 
 const Cart = createContext();
@@ -10,17 +11,24 @@ const products = [...Array(20)].map(() => ({
     id: faker.datatype.uuid(),
     name: faker.commerce.productName(),
     price: faker.commerce.price(),
-    image: faker.random.image(),
-    inStock: faker.random.arrayElement([0, 3, 5, 6, 7]),
+    image: faker.image.food(),
+    inStock: faker.helpers.arrayElement([0, 3, 5, 6, 7]),
     fastDelivery: faker.datatype.boolean(),
-    ratings: faker.rando.arrayElement([1, 2, 3, 4, 5]),
+    ratings: faker.helpers.arrayElement([1, 2, 3, 4, 5]),
 }));
 
-console.log(products);
+const [ state, dispatch ] = useReducer(cartReducer, {
+    products: products,
+    cart: []
+});
 
-  return <Cart.Provider>
+  return <Cart.Provider value={{ state, dispatch }}>
     {children}
   </Cart.Provider>
 }
 
-export default Context
+export default Context;
+
+export const CartState = () => {
+    return useContext(Cart);
+}
